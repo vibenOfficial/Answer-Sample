76efa47392dc86de73c9e51745015528437ca888
vibenOfficial <101676103+vibenOfficial@users.noreply.github.com>
2022-03-21 16:12:20 -0400

    Added ✨
    Added "main.py".

---
+++ main.py
@@ -0,0 +1,18 @@
+# This file doesn't have any license and can be used for your python project.
+
+answer = True
+
+print("\nAnswer Sample")
+
+print('\nCurrent answers are: yes, no\n')
+
+answer == input('Answer > ')
+
+if answer == 'yes':
+ print('You said yes.')
+
+elif answer == 'no':
+ print('You said no.')
+
+else:
+ print("Invalid Answer")
